7865885dc440cef2111f3186b218f4e7e3e71520
BuzVan <51021106+BuzVan@users.noreply.github.com>
2020-02-11 21:30:26 +0300

    Lab3 has been completed

--- app/src/main/java/com/example/theatres_app/MainActivity.java
+++ app/src/main/java/com/example/theatres_app/MainActivity.java
@@ -6,16 +6,69 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.theatres_app.model.Theater;
+
 public class MainActivity extends AppCompatActivity {
+
+    String[] theaterName;
+    String[] theaterAddress;
+    String[] theaterVk;
+    String[] theaterSite;
+    String[] theaterTel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        theaterName = getResources().getStringArray(R.array.theater_names);
+        theaterAddress = getResources().getStringArray(R.array.theater_address);
+        theaterVk = getResources().getStringArray(R.array.theater_vk);
+        theaterSite = getResources().getStringArray(R.array.theater_site);
+        theaterTel = getResources().getStringArray(R.array.theater_tel);
     }
 
-    public void CardClick(View view) {
+    public void onCardClick(View view) {
+
+
         Intent intent = new Intent(getApplicationContext(), TheaterActivity.class);
+        int number = (view.getId()==R.id.theaterTUZ)? 0: (view.getId()==R.id.theaterDrama)?1:2;
+        Theater theater = getTheaterByNum(number);
+        intent.putExtra("theater", theater);
+
+
         startActivity(intent);
+    }
+    private Theater getTheaterByNum(int num){
+        Theater theater = (num==0)? getTUZ():
+                (num==1)? getDram():
+                        getDol();
+        //создание элемента Theater theater с помощью конструктора
+        int id = (num==0)?R.drawable.tuz_image:(num==1)? R.drawable.drama_image:R.drawable.dolls_image;
+        theater.setImageId(id);
+        return theater;
+    }
+    private Theater getTUZ(){
+        return new Theater(
+                getResources().getString(R.string.theaterTUZ_name),
+                getResources().getString(R.string.theaterTUZ_address),
+                getResources().getString(R.string.theaterTUZ_site),
+                getResources().getString(R.string.theaterTUZ_vk),
+                getResources().getString(R.string.theaterTUZ_tel));
+    }
+    private Theater getDram(){
+        return new Theater(
+                getResources().getString(R.string.theaterDrama_name),
+                getResources().getString(R.string.theaterDrama_address),
+                getResources().getString(R.string.theaterDrama_site),
+                getResources().getString(R.string.theaterDrama_vk),
+                getResources().getString(R.string.theaterDrama_tel));
+    }
+    private Theater getDol(){
+        return new Theater(
+                getResources().getString(R.string.theaterDol_name),
+                getResources().getString(R.string.theaterDol_address),
+                getResources().getString(R.string.theaterDol_site),
+                getResources().getString(R.string.theaterDol_vk),
+                getResources().getString(R.string.theaterDol_tel));
     }
 }
--- app/src/main/java/com/example/theatres_app/TheaterActivity.java
+++ app/src/main/java/com/example/theatres_app/TheaterActivity.java
@@ -3,12 +3,40 @@ package com.example.theatres_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.theatres_app.model.Theater;
 
 public class TheaterActivity extends AppCompatActivity {
 
+    private Theater theater;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Bundle intent = getIntent().getExtras();
+        assert intent != null;
+        theater = (Theater) intent.getSerializable("theater");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theater);
+
+        ImageView theaterImageView = findViewById(R.id.theater_image_view_details);
+        theaterImageView.setImageResource(theater.getImageId());
+
+        TextView theaterNameTextView = findViewById(R.id.theater_name_text_view_details);
+        theaterNameTextView.setText(theater.getName());
+
+        TextView theaterVKTextView = findViewById(R.id.theater_vk_text_view);
+        theaterVKTextView.setText(theater.getVk());
+
+        TextView theaterSiteTextView = findViewById(R.id.theater_site_text_view);
+        theaterSiteTextView.setText(theater.getSite());
+
+        TextView theaterTelTextView = findViewById(R.id.theater_tel_text_view);
+        theaterTelTextView.setText(theater.getTel());
+
+        TextView theaterAddressTextView = findViewById(R.id.theater_address_text_view);
+        theaterAddressTextView.setText(theater.getAddress());
     }
 }
